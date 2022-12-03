87308b6382ff56905659687da2a054bf3c96fcb1
EnabledFish <enabledfish@gmail.com>
2022-12-03 23:09:42 +0800

    📦Boot: Add `Uefi` support

---
+++ Bootloader/Uefi/src/main.rs
@@ -0,0 +1,13 @@
+#![no_main]
+#![no_std]
+#![feature(abi_efiapi)]
+
+use uefi::prelude::*;
+use uefi_services::println;
+
+#[entry]
+fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    uefi_services::init(&mut system_table).unwrap();
+    println!("Hello, world!");
+    Status::SUCCESS
+}
